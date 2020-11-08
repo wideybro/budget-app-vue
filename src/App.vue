7f70c349b0c1881af87a3854ab1f5a16e2ddb281
wideybro <artem.a.babenko@gmail.com>
2020-11-08 17:37:30 +0300

    feat: add budget list

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BudgetList :list="list" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BudgetList from "@/components/BudgetList";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BudgetList,
+  },
+  data: () => ({
+    list: {
+      1: {
+        type: "INCOME",
+        value: 100,
+        comment: "Some comment",
+        id: 1,
+      },
+      2: {
+        type: "OUTCOME",
+        value: -50,
+        comment: "Some outcome comment",
+        id: 2,
+      },
+    },
+  }),
+};
 </script>
 
 <style>
